b23107d3b07122cb3a14493ce4f03e461648722c
Changmin Yi <ycm93dg@gmail.com>
2022-04-06 11:29:29 +0900

    [feat] add git config shell

---
+++ git_merge_upstream.sh
@@ -0,0 +1,5 @@
+git checkout master
+git pull master
+git fetch upstream
+git merge upstream/master
+git push
---
+++ git_set_upstream.sh
@@ -0,0 +1,2 @@
+git remote add upstream git@github.com:ultralytics/yolov5.git
+git branch --set-upstream-to=origin
