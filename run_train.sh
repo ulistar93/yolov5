9c3e8fd02f97225bf542e669fae3ce98fcd1abb4
Changmin Yi <ycm93dg@gmail.com>
2022-04-11 10:20:08 +0900

    [run] fix run_train.sh to new gray run

--- run_train.sh
+++ run_train.sh
@@ -4,13 +4,24 @@ source ~/torch1.8/bin/activate
 
 python train.py \
   --data ./data/MDSM7+smoking+ddd.yaml \
-  --weights yolov5l.pt \
+  --weights '' \
   --hyp ./data/hyps/hyp.m7smoddd.yaml \
+  --cfg ./models/yolov5s_gray.yaml \
   --device 0 \
   --epochs 100 \
   --batch-size 16 \
-  --workers 2 \
-  --name MDSM7+smoking+ddd-lowaug
+  --name MDSM7+smoking+ddd-gray
+  #--workers 4 \
+  # 2022.04.11
+
+  #--data ./data/MDSM7+smoking+ddd.yaml \
+  #--weights yolov5l.pt \
+  #--hyp ./data/hyps/hyp.m7smoddd.yaml \
+  #--device 0 \
+  #--epochs 100 \
+  #--batch-size 16 \
+  #--workers 2 \
+  #--name MDSM7+smoking+ddd-lowaug
   # 2022.04.07
 
   #--data ./data/MDSM7+smoking+ddd.yaml \
