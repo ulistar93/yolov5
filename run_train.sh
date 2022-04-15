0dc9a51e99a424507890c2514058be1f375b28ed
Changmin Yi <ycm93dg@gmail.com>
2022-04-15 18:02:45 +0900

    [run] shrink width 1/8 for gray nano

--- run_train.sh
+++ run_train.sh
@@ -6,13 +6,28 @@ python train.py \
   --data ./data/MDSM7+smoking+ddd.yaml \
   --weights '' \
   --hyp ./data/hyps/hyp.m7smoddd.yaml \
-  --cfg ./models/yolov5s_gray.yaml \
+  --cfg ./models/yolov5n_gray.yaml \
   --device 0 \
-  --epochs 100 \
-  --batch-size 4 \
-  --name MDSM7+smoking+ddd-gray_s
+  --imgsz 320 \
+  --batch-size 16 \
+  --workers 2 \
+  --epochs 300 \
+  --name MDSM7+smoking+ddd-gray_n_w8
+  #--cfg ./models/yolov5n.yaml \
+  #--name MDSM7+smoking+ddd-rgb_n_nopt
+  # 2022.04.15
+  
+  #--data ./data/MDSM7+smoking+ddd.yaml \
+  #--weights '' \
+  #--hyp ./data/hyps/hyp.m7smoddd.yaml \
+  #--cfg ./models/yolov5s_gray.yaml \
+  #--device 0 \
+  #--epochs 100 \
+  #--batch-size 4 \
+  #--name MDSM7+smoking+ddd-gray_s
   #--workers 4 \
   # 2022.04.11
+
 
   #--data ./data/MDSM7+smoking+ddd.yaml \
   #--weights yolov5l.pt \
